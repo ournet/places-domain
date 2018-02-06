191ad3199547e2a4ed4ae471568437de0a0803e5
Dumitru Cantea <canteadumitru@gmail.com>
2018-02-06 11:30:15 +0200

    v0.1.4

--- src/interactors/PlaceRepository.ts
+++ src/interactors/PlaceRepository.ts
@@ -3,7 +3,7 @@ import { IRepository, IReadRepository, IWriteRepository, RepAccessOptions } from
 import { IPlace, IOldPlaceId } from '../entities/Place';
 
 export interface IPlaceReadRepository extends IReadRepository<number, IPlace> {
-    search(data: { query: string, country: string, limit: number }, options?: RepAccessOptions<IPlace>): Promise<IPlace[]>
+    search(data: { query: string, country: string, limit: number, type?: string }, options?: RepAccessOptions<IPlace>): Promise<IPlace[]>
     getAdmin1s(data: { country: string, limit: number }, options?: RepAccessOptions<IPlace>): Promise<IPlace[]>
     getAdmin1(data: { country: string, admin1Code: string }, options?: RepAccessOptions<IPlace>): Promise<IPlace>
     getPlacesInAdmin1(data: { country: string, admin1Code: string, limit: number }, options?: RepAccessOptions<IPlace>): Promise<IPlace[]>
